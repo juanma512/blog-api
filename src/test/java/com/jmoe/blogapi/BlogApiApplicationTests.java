d1c98ba7f85b14909d062062ff8aeaf0f4e3d6e0
Juan Manuel Oviedo Exposito <juan.manuel.oviedo.exposito@ing.com>
2021-01-18 13:14:18 +0100

    Initial commit

---
+++ src/test/java/com/jmoe/blogapi/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmoe.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
